b88a95da84e10d2ce8fffbc54a4fe4d9842bcd95
Fredrik Bystam <fredrik.bystam@bontouch.com>
2016-05-10 23:50:31 +0200

    don't write password explicitly

--- api/model/bookshelf.js
+++ api/model/bookshelf.js
@@ -12,8 +12,8 @@ if (db === 'heroku_dev') {
     client: 'pg',
     connection: {
       host     : 'ec2-54-228-219-2.eu-west-1.compute.amazonaws.com',
-      user     : 'scemjrevdeifas',
-      password : 'CKeOr_s8VobQJaHSf_hooCYooV',
+      user     : 'hgznctmjjryuvs',
+      password : process.env.LANDET_DEV_DB_PW,
       database : 'd403dmetsr9t9q',
       port     : 5432,
       charset  : 'utf8'
