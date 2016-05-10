2ce75c58535ec05b2e52e54bfce96e6cda51a95d
Fredrik Bystam <fredrik.bystam@bontouch.com>
2016-05-10 23:10:24 +0200

    prepare the usage of postgres on heroku

--- api/model/bookshelf.js
+++ api/model/bookshelf.js
@@ -1,13 +1,31 @@
 'use strict'
 
-const knex = require('knex')({
-  client: 'sqlite3',
-  useNullAsDefault: true,
-  connection: {
-    filename: './database/db.sqlite3'
-  }
-})
+let knexConfig = {};
 
+const db = process.env.LANDET_DB
+if (db == 'heroku_dev') {
+  console.log('using heroku dev db');
+  knexConfig = {
+    client: 'pg',
+    connection: {
+      host     : 'ec2-54-228-219-2.eu-west-1.compute.amazonaws.com',
+      user     : 'scemjrevdeifas',
+      password : 'CKeOr_s8VobQJaHSf_hooCYooV',
+      database : 'd403dmetsr9t9q',
+      port     : 5432,
+      charset  : 'utf8'
+    }
+  };
+} else {
+  knexConfig = {
+    client: 'sqlite3',
+    useNullAsDefault: true,
+    connection: {
+      filename: './database/db.sqlite3'
+  };
+}
+
+const knex = require('knex')(knexConfig)
 const bookshelf = require('bookshelf')(knex);
 
 module.exports = {
