9547e345c4925d4f52fefbb575d83dff93617726
Fredrik Bystam <fredrik.bystam@bontouch.com>
2016-06-06 14:51:00 +0200

    use the common error handling in the auth middleware as well

--- api/middleware/auth.js
+++ api/middleware/auth.js
@@ -4,26 +4,14 @@ const sessions = require('../model/sessions');
 
 const errors = require('../util/errors');
 
-const kUnauthorized = {
-  status: 401,
-  message: '401: Unauthorized'
-};
-
 function authenticate(req, res, next) {
   let token = req.get('Authorization') || '';
   token = token.replace('Basic ', '');
-  if (token.length === 0) {
-    return res.status(401).json(kUnauthorized);
-  }
 
   sessions.userWithSessionToken(token).then(function (user) {
-    if (!user) {
-      return res.status(401).json(kUnauthorized);
-    }
-
     req.user = user;
     next();
-  });
+  }).catch(errors.HttpHandler(res));
 }
 
 module.exports = {
