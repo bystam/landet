0d0b8ab25709cbd1e02d666423bd1a8ba8164fb6
Fredrik Bystam <fredrik.bystam@bontouch.com>
2016-06-15 23:01:54 +0200

    user heroku URL

--- iOS/Landet/Landet/AppDelegate.swift
+++ iOS/Landet/Landet/AppDelegate.swift
@@ -13,7 +13,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
 
-        HttpClient.debugHost = "http://10.0.1.103:3000"
+//        HttpClient.debugHost = "http://10.0.1.103:3000"
 
         installSession()
         performLaunchCalls()
