0786645db3e3a6904bc9c278f1c54210cf76be4f
Fredrik Bystam <fredrik.bystam@bontouch.com>
2016-06-27 18:48:29 +0200

    makes topic header item sizes more dynamic, and set the correct collapsed topic label text

--- ios/Landet/Landet/TopicsHeaderCollectionViewCell.swift
+++ ios/Landet/Landet/TopicsHeaderCollectionViewCell.swift
@@ -7,5 +7,10 @@ import UIKit
 class TopicsHeaderCollectionViewCell: UICollectionViewCell {
 
     static let reuseIdentifier: String = "TopicsHeaderCollectionViewCell"
-    
+
+    @IBOutlet weak var titleLabel: UILabel!
+
+    func configure(topic topic: Topic) {
+        titleLabel.text = topic.title
+    }
 }
--- ios/Landet/Landet/TopicsHeaderViewController.swift
+++ ios/Landet/Landet/TopicsHeaderViewController.swift
@@ -6,24 +6,44 @@ import UIKit
 
 class TopicsHeaderViewController: UIViewController {
 
-    var topics = [Topic]() {
-        didSet { collectionView.reloadData() }
-    }
-
-    @IBOutlet weak var collectionView: UICollectionView!
     @IBOutlet weak var currentTopicLabel: UILabel!
+    @IBOutlet weak var collectionView: UICollectionView! {
+        didSet {
+            let flowLayout = collectionView.collectionViewLayout as! UICollectionViewFlowLayout
+            flowLayout.itemSize = CGSize(width: itemWidth, height: 249)
+        }
+    }
 
     lazy var defaultHeight: CGFloat = {
         return self.collectionView.bounds.height
     }()
     let minHeight: CGFloat = 60
+    let itemWidth = UIScreen.mainScreen().bounds.width
+
+    var topics = [Topic]() {
+        didSet {
+            collectionView.reloadData()
+            currentTopicLabel.text = topics.first?.title
+        }
+    }
+    var currentTopic: Topic? {
+        let offsetMiddle = collectionView.contentOffset.x + itemWidth/2
+        let index = Int(offsetMiddle / itemWidth)
+        guard index >= 0 && index < topics.count else { return nil }
+        return topics[index]
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         LandetCollectionViewStyle.setup(collectionView, cells: [.TopicHeader, .Spinner])
 
         collectionView.dataSource = self
+        collectionView.delegate = self
     }
+}
+
+// Public
+extension TopicsHeaderViewController {
 
     func respondToHeight(height: CGFloat) {
         let shrink = max(defaultHeight - height, CGFloat(0))
@@ -49,6 +69,16 @@ extension TopicsHeaderViewController: UICollectionViewDataSource {
             cell.spin()
             return cell
         }
-        return collectionView.dequeueLandetCell(.TopicHeader, forIndexPath: indexPath)
+        let cell: TopicsHeaderCollectionViewCell = collectionView.dequeueLandetCell(.TopicHeader, forIndexPath: indexPath)
+        cell.configure(topic: topics[indexPath.item])
+        return cell
+    }
+}
+
+
+extension TopicsHeaderViewController: UICollectionViewDelegate {
+
+    func scrollViewDidEndDecelerating(scrollView: UIScrollView) {
+        currentTopicLabel.text = currentTopic?.title
     }
 }
