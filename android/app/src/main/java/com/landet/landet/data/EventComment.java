adca70417e6bc236593de792376b5aefec3acdde
Jonas Sköld <jonas.skold@bontouch.com>
2016-07-06 18:28:24 +0200

    Added more fields to the event comment class

--- android/app/src/main/java/com/landet/landet/data/EventComment.java
+++ android/app/src/main/java/com/landet/landet/data/EventComment.java
@@ -3,14 +3,30 @@ package com.landet.landet.data;
 import android.os.Parcel;
 import android.os.Parcelable;
 
+import com.google.gson.annotations.SerializedName;
+
+import org.joda.time.DateTime;
+
 public class EventComment extends DataWithId implements Parcelable {
     private String text;
+    @SerializedName("comment_time")
+    private DateTime dateTime;
+    private User author;
 
     public EventComment() {}
 
     public String getText() {
         return text;
     }
+
+    public DateTime getDateTime() {
+        return dateTime;
+    }
+
+    public User getAuthor() {
+        return author;
+    }
+
 
     @Override
     public int describeContents() { return 0; }
@@ -19,11 +35,15 @@ public class EventComment extends DataWithId implements Parcelable {
     public void writeToParcel(Parcel dest, int flags) {
         super.writeToParcel(dest, flags);
         dest.writeString(this.text);
+        dest.writeSerializable(this.dateTime);
+        dest.writeParcelable(this.author, flags);
     }
 
     protected EventComment(Parcel in) {
         super(in);
         this.text = in.readString();
+        this.dateTime = (DateTime) in.readSerializable();
+        this.author = in.readParcelable(User.class.getClassLoader());
     }
 
     public static final Creator<EventComment> CREATOR = new Creator<EventComment>() {
