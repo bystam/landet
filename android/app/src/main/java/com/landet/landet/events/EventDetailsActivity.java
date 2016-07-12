5261d9d77818df64aa53c62c83f83dba54591a5c
Shayan Effati <shayan@effati.com>
2016-07-12 20:31:28 +0200

    nicer collapsing toolbar in event details.

--- android/app/src/main/java/com/landet/landet/events/EventDetailsActivity.java
+++ android/app/src/main/java/com/landet/landet/events/EventDetailsActivity.java
@@ -4,7 +4,6 @@ import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.v7.app.ActionBar;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
@@ -41,6 +40,7 @@ public class EventDetailsActivity extends BaseActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setTheme(R.style.LandetThemeTransparentBar);
         setContentView(R.layout.activity_event_details);
 
         mModel = new EventModel(mBackend);
@@ -49,10 +49,9 @@ public class EventDetailsActivity extends BaseActivity {
 
         setupLayout();
 
-        ActionBar actionBar = getSupportActionBar();
-        if (actionBar != null) {
-            actionBar.setDisplayHomeAsUpEnabled(true);
-        }
+        final Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         mAdapter = new EventCommentAdapter(this);
         RecyclerView view = (RecyclerView) findViewById(R.id.comments);
