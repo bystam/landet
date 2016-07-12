e2c512d742b1fdc731e2924dcf62c98cbadbd876
Fredrik Bystam <fredrik.bystam@gmail.com>
2016-07-12 22:39:00 +0200

    create an image cache for images loaded from the server

--- ios/Landet/Landet/ImageLoader.swift
+++ ios/Landet/Landet/ImageLoader.swift
@@ -6,13 +6,25 @@ import UIKit
 
 class ImageLoader {
 
+    static let cache = NSCache()
+
     static func loadImage(url: NSURL, completion: (image: UIImage?) -> Void) {
+        if let cachedImage = cache.objectForKey(url) as? UIImage {
+            completion(image: cachedImage)
+            return
+        }
+
         let task = NSURLSession.sharedSession().dataTaskWithURL(url) { (data, res, error) in
 
             if let data = data where data.length > 0 {
                 let image = UIImage(data: data)
 
                 Async.main {
+
+                    if let image = image {
+                        cache.setObject(image, forKey: url)
+                    }
+
                     completion(image: image)
                 }
             }
