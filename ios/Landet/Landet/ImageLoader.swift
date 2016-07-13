c7ae1828ade7a9f94409fd884def1df8d4710ce2
Fredrik Bystam <fredrik.bystam@gmail.com>
2016-07-13 18:18:00 +0200

    use the built-in url resource cache instead of a self-written one

--- ios/Landet/Landet/ImageLoader.swift
+++ ios/Landet/Landet/ImageLoader.swift
@@ -6,25 +6,15 @@ import UIKit
 
 class ImageLoader {
 
-    static let cache = NSCache()
-
     static func loadImage(url: NSURL, completion: (image: UIImage?) -> Void) {
-        if let cachedImage = cache.objectForKey(url) as? UIImage {
-            completion(image: cachedImage)
-            return
-        }
+        let request = NSURLRequest(URL: url, cachePolicy: .ReturnCacheDataElseLoad, timeoutInterval: 20.0)
 
-        let task = NSURLSession.sharedSession().dataTaskWithURL(url) { (data, res, error) in
+        let task = NSURLSession.sharedSession().dataTaskWithRequest(request) { (data, res, error) in
 
             if let data = data where data.length > 0 {
                 let image = UIImage(data: data)
 
                 Async.main {
-
-                    if let image = image {
-                        cache.setObject(image, forKey: url)
-                    }
-
                     completion(image: image)
                 }
             }
